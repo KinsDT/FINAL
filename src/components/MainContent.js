f705d28b237f8b3e3d94a3fa5e658edc2cfb507d
KinsDT <Kinshuk.Goswami@kimbal.io>
2025-07-11 10:17:27 +0530

    munir

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -105,7 +105,7 @@ export default function MainContent({
   );
 }
 */
-
+/*
 import React from 'react';
 import { Layout, Select, DatePicker } from 'antd';
 import AggregationSelector from './AggregationSelector';
@@ -212,6 +212,137 @@ export default function MainContent({
             placeholder="To Date"
             value={toDate}
             onChange={date => setToDate(date)}
+          />
+        </div>
+      </div>
+
+      <AggregationSelector aggregation={aggregation} setAggregation={setAggregation} />
+
+      <MeterMap
+        center={center}
+        locations={filteredLocations}
+        selectedParam={selectedParam}
+        meterParamData={meterParamData}
+        aggregation={aggregation}
+        heatmapAgg={heatmapAgg}
+        heatmapData={heatmapData}
+        setHeatmapAgg={setHeatmapAgg}
+        fromDate={fromDate}
+        toDate={toDate}
+      />
+    </Content>
+  );
+}
+*/
+
+import React from 'react';
+import { Layout, Select, DatePicker } from 'antd';
+import AggregationSelector from './AggregationSelector';
+import MeterMap from './MeterMap';
+import { parameterOptions } from '../utils/constants';
+
+const { Option } = Select;
+const { Content } = Layout;
+
+export default function MainContent({
+  center,
+  locations,
+  selectedArea,
+  setSelectedArea,
+  selectedParam,
+  setSelectedParam,
+  meterParamData,
+  aggregation,
+  setAggregation,
+  heatmapAgg,
+  setHeatmapAgg,
+  heatmapData,
+  fromDate,
+  toDate,
+  setFromDate,
+  setToDate
+}) {
+  const availableAreas = [...new Set(locations.map(loc => loc.AREA).filter(Boolean))];
+
+  const filteredLocations = selectedArea
+    ? locations.filter(loc => loc.AREA === selectedArea)
+    : locations;
+
+  return (
+    <Content style={{ padding: '20px' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '0 10px 20px',
+          flexWrap: 'wrap'
+        }}
+      >
+        <h1
+          style={{
+            color: '#27272A',
+            fontFamily: 'GT Walsheim Pro',
+            fontSize: 20,
+            fontWeight: 500
+          }}
+        >
+          DT Performance Map
+        </h1>
+
+        <div style={{ display: 'flex', alignItems: 'center', gap: '10px', flexWrap: 'wrap' }}>
+          <Select
+            value={selectedArea}
+            onChange={value => setSelectedArea(value)}
+            placeholder="Filter by Area"
+            style={{
+              border: '1px solid #DDDDE3',
+              background: '#FFF',
+              fontFamily: 'GT Walsheim Pro',
+              minWidth: 180
+            }}
+            allowClear
+          >
+            {availableAreas.map(area => (
+              <Option key={area} value={area}>
+                {area}
+              </Option>
+            ))}
+          </Select>
+
+          <Select
+            value={selectedParam}
+            onChange={value => setSelectedParam(value)}
+            placeholder="Filter by Monitoring Parameter"
+            style={{
+              border: '1px solid #DDDDE3',
+              background: '#FFF',
+              fontFamily: 'GT Walsheim Pro',
+              minWidth: 200
+            }}
+            allowClear
+          >
+            {parameterOptions.map(param => (
+              <Option key={param} value={param}>
+                {param}
+              </Option>
+            ))}
+          </Select>
+
+          <DatePicker
+            style={{ width: 150 }}
+            placeholder="From Date"
+            value={fromDate}
+            onChange={setFromDate}
+            allowClear
+          />
+
+          <DatePicker
+            style={{ width: 150 }}
+            placeholder="To Date"
+            value={toDate}
+            onChange={setToDate}
+            allowClear
           />
         </div>
       </div>
